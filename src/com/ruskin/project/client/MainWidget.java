7f3bdf605ec9dbcf9118d70d74edbd298eb5530d
nick <scott@nscotton.com>
2015-09-19 15:46:05 -0500

    added test image

--- src/com/ruskin/project/client/MainWidget.java
+++ src/com/ruskin/project/client/MainWidget.java
@@ -21,7 +21,7 @@ public class MainWidget implements IsWidget {
 	
 	public MainWidget() {
 		this.mainPanel = new VerticalPanel();
-		placesMap = new PlaceMapWidget(500,300, this);
+		placesMap = new PlaceMapWidget(900,500, this);
 		timePanel = new TimeWidget(this);
 		forum = new Forum(this);		
 		
@@ -51,9 +51,9 @@ public class MainWidget implements IsWidget {
 	
 	}
 
-//	public final PlaceMapWidget getMap() {
-//		return placesMap;
-//	}
+	public final PlaceMapWidget getMap() {
+		return placesMap;
+	}
 	
 	public TimeWidget getTimeWidget() {
 		return timePanel;
